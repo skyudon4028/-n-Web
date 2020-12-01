66521c8a3b6a5dda59e5839d79221ac6b09390f6
longkudo4770 <nguyenhailong1412@gmail.com>
2020-12-01 21:20:00 +0700

    Update

--- upload.php
+++ upload.php
@@ -1,40 +1,84 @@
 <?php
 	session_start();
-	$message = ''; 
+	include('db.php');
+	$message = '';
+	$target_dir = "uploads/";
 
 	if(isset($_POST["submit"])) {
-		$target_dir = "uploads/";
-		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-		$uploadOk = 1;
-		$fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-		$allowedfileTypes = array('jpg', 'gif', 'png', 'rar', 'zip', 'txt', 'xls', 'doc', 'docx');
-		
-		if ($fileType != "jpg" && $fileType != "gif" && $fileType != "png" && $fileType != "rar" && $fileType != "zip" && $fileType != "txt" 
-		&& $fileType != "xls" && $fileType != "doc" && $fileType != "docx") {
-			$uploadOk = 0;
-		} else {
+		if (!empty($_FILES["fileToUpload"]["name"])) {
+			$fileName = basename($_FILES["fileToUpload"]["name"]);
+			$target_file = $target_dir . $fileName;
 			$uploadOk = 1;
-		}
-	}
-
-	if (file_exists($target_file)) {
-		$message = "Sorry, file already exists.";
-	} else {
-		if ($_FILES["fileToUpload"]["size"] > 5000000) {
-			$message = "Sorry, your file is too large.";
-		} else {
-			if ($uploadOk == 0) {
-				$message = "Sorry, only ". implode(', ', $allowedfileTypes). " files are allowed.";
+			$fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+			$allowedfileTypes = array('jpg', 'gif', 'png', 'rar', 'zip', 'txt', 'xls', 'doc', 'docx', 'pdf');
+			
+			if (!in_array($fileType, $allowedfileTypes)) {
+				$uploadOk = 0;
 			} else {
-				if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-					$message = "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+				$uploadOk = 1;
+			}
+			
+			if (file_exists($target_file)) {
+				$message = "Sorry, file already exists.";
+			} else {
+				if ($_FILES["fileToUpload"]["size"] > 5000000) {
+					$message = "Sorry, your file is too large.";
 				} else {
-					$message = "Sorry, there was an error uploading your file.";
+					if ($uploadOk == 0) {
+						$message = "Sorry, only ". implode(', ', $allowedfileTypes). " files are allowed.";
+					} else {
+						class Upload {
+							private $db;
+							private $connection;
+
+							function __construct() {
+								//constructor call    
+								$this->db = new DB_Connection();
+								$this->connection = $this->db->get_connection();
+							}
+							
+							public function upload_file($file_name) {
+								//set default time zone
+								date_default_timezone_set("Asia/Ho_Chi_Minh");   
+								//VietNam time (GMT+7)
+								// get date and time
+								$date_upload = date('Y-m-d H:i:s');
+								
+								//saving the file 
+								$query = "INSERT INTO fileinfo(file_name, date_upload) VALUES('$file_name', '$date_upload')";
+								$is_uploaded = mysqli_query($this->connection,$query);
+								
+								mysqli_close($this->connection);
+							}
+						}
+						
+						$load_file = new Upload();
+						
+						if (is_array($_FILES)) { 
+							if (is_uploaded_file($_FILES['fileToUpload']['tmp_name'])) {
+								$sourcePath = $_FILES['fileToUpload']['tmp_name'];
+								$targetPath = "uploads/" .$_FILES['fileToUpload']['name'];        
+								if (move_uploaded_file($sourcePath,$targetPath)) {
+									$insert = $load_file-> upload_file($_FILES['fileToUpload']['name']);
+									if (!$insert) {
+										$message = "The file " .$fileName. " has been uploaded successfully.";
+									} else {
+										$message = "File upload failed, please try again.";
+									}
+								} else {
+									$message = "Sorry, there was an error uploading your file.";
+								}
+							}
+						}
+					}
 				}
 			}
+		} else {
+			$message = "Please select a file to upload.";
 		}
 	}
 	
 	$_SESSION['message'] = $message;
+	$_SESSION['table'] = $mess_table;
 	header("Location: nopbt.php");
 ?>
