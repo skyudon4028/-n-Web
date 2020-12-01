b2ff842dcefc828d7101be7e484edf129941f56b
longkudo4770 <nguyenhailong1412@gmail.com>
2020-12-01 23:30:50 +0700

    Update trang nop bt

--- nopbt.php
+++ nopbt.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	
-	$conn = mysqli_connect("127.0.0.1", "root", "", "uploadfile");
+	$conn = mysqli_connect("127.0.0.1", "root", "", "projectweb");
 	if ($conn->connect_error) {
 		die("Connection failed:" .$conn->connect_error);
 	}
